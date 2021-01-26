03b7c1a1461abe99d0b4ef2fb99372706139b024
Chen Prom <chenprom@Chens-MacBook-Pro.local>
2021-01-26 10:14:00 -0600

    Add spawning enemies and powerups

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,6 +8,7 @@ public class Enemy : MonoBehaviour
     public float speed;
     public Rigidbody enemyRb;
     public GameObject player;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -21,5 +22,10 @@ public class Enemy : MonoBehaviour
         Vector3 lookDirection = (player.transform.position - transform.position).normalized;
 
         enemyRb.AddForce(lookDirection * speed);
+
+        if(transform.position.y < -10)
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,19 +5,48 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public GameObject powerupPrefab;
     private float spawnRange = 9.0f;
+    public int enemyCount;
+    public int waveNumber;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
-
-        Instantiate(enemyPrefab, GenerateSpawnPostion(), enemyPrefab.transform.rotation);
+        SpawnEnemyWave(waveNumber);
+        PowerUpSpawn(waveNumber);
+            
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        if (enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+            PowerUpSpawn(waveNumber);
+        }
+    }
+
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPostion(), enemyPrefab.transform.rotation);
+        }
+    }
+
+    void PowerUpSpawn(int waveNumber)
+    {
+        for (int i = 0; i < waveNumber ; i++)
+        {
+            Instantiate(powerupPrefab, GenerateSpawnPostion(), powerupPrefab.transform.rotation);
+        }
     }
 
     private Vector3 GenerateSpawnPostion()
